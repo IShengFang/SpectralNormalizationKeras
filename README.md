386fa324b5da831ac4fd0b9906a597a69763c73f
ishengfang <nf0126@google.com>
2018-06-03 21:24:56 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# SpectralNormalizationKeras
-Spectral Normalization for Keras Dense and Convolution Layers
+Spectral Normalization for Keras
+================================
+The **simple** Keras implementation of ICLR 2018 paper, [Spectral Normalization for Generative Adversarial Networks](https://openreview.net/forum?id=B1QRgziT-)
+
+How to use?
+----
+1. Move SpectralNormalizationKeras.py in your dir
+2. Import these layer class
+``` python
+from SpectralNormalizationKeras import DenseSN, ConvSN1D, ConvSN2D, ConvSN3D
+```
+3. Use these layers in your discriminator as usual
+
+
+
+Issue
+-----
+1. added Cifar 100 example code.
---
+++ SpectralNormalizationKeras.py
@@ -0,0 +1,429 @@
+from keras import backend as K
+from keras.engine import *
+from keras.legacy import interfaces
+from keras import activations
+from keras import initializers
+from keras import regularizers
+from keras import constraints
+from keras.utils.generic_utils import func_dump
+from keras.utils.generic_utils import func_load
+from keras.utils.generic_utils import deserialize_keras_object
+from keras.utils.generic_utils import has_arg
+from keras.utils import conv_utils
+from keras.legacy import interfaces
+
+class DenseSN(Dense):
+    def _l2normalize(self, v, eps=1e-12):
+        return v / (K.sum(v ** 2) ** 0.5 + eps)
+    
+    def power_iteration(self, u, W):
+        '''
+        Accroding the paper, we only need to do power iteration one time.
+        '''
+        v = self._l2normalize(K.dot(u, K.transpose(W)))
+        u = self._l2normalize(K.dot(v, W))
+        return u, v
+    
+    def build(self, input_shape):
+        assert len(input_shape) >= 2
+        input_dim = input_shape[-1]
+
+        self.kernel = self.add_weight(shape=(input_dim, self.units),
+                                      initializer=self.kernel_initializer,
+                                      name='kernel',
+                                      regularizer=self.kernel_regularizer,
+                                      constraint=self.kernel_constraint)
+        #Spectral Normalization
+        #Get kernel tensor shape
+        W_shape = self.kernel.shape.as_list()
+        #Flatten the Tensor
+        W_reshaped = K.reshape(self.kernel, [-1, W_shape[-1]])
+        #initialize u vector
+        u = K.random_normal_variable([1, W_shape[-1]], 0 ,1)
+
+        u,v = self.power_iteration(u, W_reshaped)
+        #Calculate Sigma
+        sigma=K.dot(v, W_reshaped)
+        sigma=K.dot(sigma, K.transpose(u))
+        #normalize it
+        W_bar = W_reshaped / sigma
+        #reshape weight tensor
+        W_bar = K.reshape(W_bar, W_shape)
+        #update weitht
+        self.kernel = W_bar
+        if self.use_bias:
+            self.bias = self.add_weight(shape=(self.units,),
+                                        initializer=self.bias_initializer,
+                                        name='bias',
+                                        regularizer=self.bias_regularizer,
+                                        constraint=self.bias_constraint)
+        else:
+            self.bias = None
+        self.input_spec = InputSpec(min_ndim=2, axes={-1: input_dim})
+        self.built = True
+        
+class _ConvSN(Layer):
+
+    def __init__(self, rank,
+                 filters,
+                 kernel_size,
+                 strides=1,
+                 padding='valid',
+                 data_format=None,
+                 dilation_rate=1,
+                 activation=None,
+                 use_bias=True,
+                 kernel_initializer='glorot_uniform',
+                 bias_initializer='zeros',
+                 kernel_regularizer=None,
+                 bias_regularizer=None,
+                 activity_regularizer=None,
+                 kernel_constraint=None,
+                 bias_constraint=None,
+                 spectral_normalization=True,
+                 **kwargs):
+        super(_ConvSN, self).__init__(**kwargs)
+        self.rank = rank
+        self.filters = filters
+        self.kernel_size = conv_utils.normalize_tuple(kernel_size, rank, 'kernel_size')
+        self.strides = conv_utils.normalize_tuple(strides, rank, 'strides')
+        self.padding = conv_utils.normalize_padding(padding)
+        self.data_format = conv_utils.normalize_data_format(data_format)
+        self.dilation_rate = conv_utils.normalize_tuple(dilation_rate, rank, 'dilation_rate')
+        self.activation = activations.get(activation)
+        self.use_bias = use_bias
+        self.kernel_initializer = initializers.get(kernel_initializer)
+        self.bias_initializer = initializers.get(bias_initializer)
+        self.kernel_regularizer = regularizers.get(kernel_regularizer)
+        self.bias_regularizer = regularizers.get(bias_regularizer)
+        self.activity_regularizer = regularizers.get(activity_regularizer)
+        self.kernel_constraint = constraints.get(kernel_constraint)
+        self.bias_constraint = constraints.get(bias_constraint)
+        self.input_spec = InputSpec(ndim=self.rank + 2)
+        self.spectral_normalization = spectral_normalization
+        self.u = None
+        
+    def _l2normalize(self, v, eps=1e-12):
+        return v / (K.sum(v ** 2) ** 0.5 + eps)
+    
+    def power_iteration(self, u, W):
+        '''
+        Accroding the paper, we only need to do power iteration one time.
+        '''
+        v = self._l2normalize(K.dot(u, K.transpose(W)))
+        u = self._l2normalize(K.dot(v, W))
+        return u, v
+    def build(self, input_shape):
+        if self.data_format == 'channels_first':
+            channel_axis = 1
+        else:
+            channel_axis = -1
+        if input_shape[channel_axis] is None:
+            raise ValueError('The channel dimension of the inputs '
+                             'should be defined. Found `None`.')
+        input_dim = input_shape[channel_axis]
+        kernel_shape = self.kernel_size + (input_dim, self.filters)
+
+        self.kernel = self.add_weight(shape=kernel_shape,
+                                      initializer=self.kernel_initializer,
+                                      name='kernel',
+                                      regularizer=self.kernel_regularizer,
+                                      constraint=self.kernel_constraint)
+
+        #Spectral Normalization
+        if self.spectral_normalization:
+            #Spectral Normalization
+            #Get kernel tensor shape
+            W_shape = self.kernel.shape.as_list()
+            #Flatten the Tensor
+            W_reshaped = K.reshape(self.kernel, [-1, W_shape[-1]])
+            #initialize u vector
+            if self.u is None:
+                self.u = K.random_normal_variable([1, W_shape[-1]], 0 ,1)
+            
+            u,v = self.power_iteration(self.u, W_reshaped)
+            #Calculate Sigma
+            #u^T*W
+            sigma=K.dot(v, W_reshaped)
+            #u^T*W*v
+            sigma=K.dot(sigma, K.transpose(u))
+            #normalize it
+            W_bar = W_reshaped / sigma
+            #reshape weight tensor
+            W_bar = K.reshape(W_bar, W_shape)
+            #update weight
+            self.kernel = W_bar
+        
+        if self.use_bias:
+            self.bias = self.add_weight(shape=(self.filters,),
+                                        initializer=self.bias_initializer,
+                                        name='bias',
+                                        regularizer=self.bias_regularizer,
+                                        constraint=self.bias_constraint)
+        else:
+            self.bias = None
+        # Set input spec.
+        self.input_spec = InputSpec(ndim=self.rank + 2,
+                                    axes={channel_axis: input_dim})
+        self.built = True
+
+    def call(self, inputs):
+        if self.rank == 1:
+            outputs = K.conv1d(
+                inputs,
+                self.kernel,
+                strides=self.strides[0],
+                padding=self.padding,
+                data_format=self.data_format,
+                dilation_rate=self.dilation_rate[0])
+        if self.rank == 2:
+            outputs = K.conv2d(
+                inputs,
+                self.kernel,
+                strides=self.strides,
+                padding=self.padding,
+                data_format=self.data_format,
+                dilation_rate=self.dilation_rate)
+        if self.rank == 3:
+            outputs = K.conv3d(
+                inputs,
+                self.kernel,
+                strides=self.strides,
+                padding=self.padding,
+                data_format=self.data_format,
+                dilation_rate=self.dilation_rate)
+
+        if self.use_bias:
+            outputs = K.bias_add(
+                outputs,
+                self.bias,
+                data_format=self.data_format)
+
+        if self.activation is not None:
+            return self.activation(outputs)
+        return outputs
+
+    def compute_output_shape(self, input_shape):
+        if self.data_format == 'channels_last':
+            space = input_shape[1:-1]
+            new_space = []
+            for i in range(len(space)):
+                new_dim = conv_utils.conv_output_length(
+                    space[i],
+                    self.kernel_size[i],
+                    padding=self.padding,
+                    stride=self.strides[i],
+                    dilation=self.dilation_rate[i])
+                new_space.append(new_dim)
+            return (input_shape[0],) + tuple(new_space) + (self.filters,)
+        if self.data_format == 'channels_first':
+            space = input_shape[2:]
+            new_space = []
+            for i in range(len(space)):
+                new_dim = conv_utils.conv_output_length(
+                    space[i],
+                    self.kernel_size[i],
+                    padding=self.padding,
+                    stride=self.strides[i],
+                    dilation=self.dilation_rate[i])
+                new_space.append(new_dim)
+            return (input_shape[0], self.filters) + tuple(new_space)
+
+    def get_config(self):
+        config = {
+            'rank': self.rank,
+            'filters': self.filters,
+            'kernel_size': self.kernel_size,
+            'strides': self.strides,
+            'padding': self.padding,
+            'data_format': self.data_format,
+            'dilation_rate': self.dilation_rate,
+            'activation': activations.serialize(self.activation),
+            'use_bias': self.use_bias,
+            'kernel_initializer': initializers.serialize(self.kernel_initializer),
+            'bias_initializer': initializers.serialize(self.bias_initializer),
+            'kernel_regularizer': regularizers.serialize(self.kernel_regularizer),
+            'bias_regularizer': regularizers.serialize(self.bias_regularizer),
+            'activity_regularizer': regularizers.serialize(self.activity_regularizer),
+            'kernel_constraint': constraints.serialize(self.kernel_constraint),
+            'bias_constraint': constraints.serialize(self.bias_constraint)
+        }
+        base_config = super(_Conv, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+    
+class ConvSN2D(Conv2D):
+    def _l2normalize(self, v, eps=1e-12):
+        return v / (K.sum(v ** 2) ** 0.5 + eps)
+    
+    def power_iteration(self, u, W):
+        '''
+        Accroding the paper, we only need to do power iteration one time.
+        '''
+        v = self._l2normalize(K.dot(u, K.transpose(W)))
+        u = self._l2normalize(K.dot(v, W))
+        return u, v
+    
+    def build(self, input_shape):
+        if self.data_format == 'channels_first':
+            channel_axis = 1
+        else:
+            channel_axis = -1
+        if input_shape[channel_axis] is None:
+            raise ValueError('The channel dimension of the inputs '
+                             'should be defined. Found `None`.')
+        input_dim = input_shape[channel_axis]
+        kernel_shape = self.kernel_size + (input_dim, self.filters)
+
+        self.kernel = self.add_weight(shape=kernel_shape,
+                                      initializer=self.kernel_initializer,
+                                      name='kernel',
+                                      regularizer=self.kernel_regularizer,
+                                      constraint=self.kernel_constraint)
+        #Spectral Normalization
+        #Get kernel tensor shape
+        W_shape = self.kernel.shape.as_list()
+        #Flatten the Tensor
+        W_reshaped = K.reshape(self.kernel, [-1, W_shape[-1]])
+        #initialize u vector
+        u = K.random_normal_variable([1, W_shape[-1]], 0 ,1)
+
+        u,v = self.power_iteration(u, W_reshaped)
+        #Calculate Sigma
+        sigma=K.dot(v, W_reshaped)
+        sigma=K.dot(sigma, K.transpose(u))
+        #normalize it
+        W_bar = W_reshaped / sigma
+        #reshape weight tensor
+        W_bar = K.reshape(W_bar, W_shape)
+        #update weitht
+        self.kernel = W_bar
+        if self.use_bias:
+            self.bias = self.add_weight(shape=(self.filters,),
+                                        initializer=self.bias_initializer,
+                                        name='bias',
+                                        regularizer=self.bias_regularizer,
+                                        constraint=self.bias_constraint)
+        else:
+            self.bias = None
+        # Set input spec.
+        self.input_spec = InputSpec(ndim=self.rank + 2,
+                                    axes={channel_axis: input_dim})
+        self.built = True
+
+class ConvSN1D(Conv1D):
+    def _l2normalize(self, v, eps=1e-12):
+        return v / (K.sum(v ** 2) ** 0.5 + eps)
+    
+    def power_iteration(self, u, W):
+        '''
+        Accroding the paper, we only need to do power iteration one time.
+        '''
+        v = self._l2normalize(K.dot(u, K.transpose(W)))
+        u = self._l2normalize(K.dot(v, W))
+        return u, v
+    
+    def build(self, input_shape):
+        if self.data_format == 'channels_first':
+            channel_axis = 1
+        else:
+            channel_axis = -1
+        if input_shape[channel_axis] is None:
+            raise ValueError('The channel dimension of the inputs '
+                             'should be defined. Found `None`.')
+        input_dim = input_shape[channel_axis]
+        kernel_shape = self.kernel_size + (input_dim, self.filters)
+
+        self.kernel = self.add_weight(shape=kernel_shape,
+                                      initializer=self.kernel_initializer,
+                                      name='kernel',
+                                      regularizer=self.kernel_regularizer,
+                                      constraint=self.kernel_constraint)
+        #Spectral Normalization
+        #Get kernel tensor shape
+        W_shape = self.kernel.shape.as_list()
+        #Flatten the Tensor
+        W_reshaped = K.reshape(self.kernel, [-1, W_shape[-1]])
+        #initialize u vector
+        u = K.random_normal_variable([1, W_shape[-1]], 0 ,1)
+
+        u,v = self.power_iteration(u, W_reshaped)
+        #Calculate Sigma
+        sigma=K.dot(v, W_reshaped)
+        sigma=K.dot(sigma, K.transpose(u))
+        #normalize it
+        W_bar = W_reshaped / sigma
+        #reshape weight tensor
+        W_bar = K.reshape(W_bar, W_shape)
+        #update weitht
+        self.kernel = W_bar
+        if self.use_bias:
+            self.bias = self.add_weight(shape=(self.filters,),
+                                        initializer=self.bias_initializer,
+                                        name='bias',
+                                        regularizer=self.bias_regularizer,
+                                        constraint=self.bias_constraint)
+        else:
+            self.bias = None
+        # Set input spec.
+        self.input_spec = InputSpec(ndim=self.rank + 2,
+                                    axes={channel_axis: input_dim})
+        self.built = True
+
+class ConvSN3D(Conv3D):
+    def _l2normalize(self, v, eps=1e-12):
+        return v / (K.sum(v ** 2) ** 0.5 + eps)
+    
+    def power_iteration(self, u, W):
+        '''
+        Accroding the paper, we only need to do power iteration one time.
+        '''
+        v = self._l2normalize(K.dot(u, K.transpose(W)))
+        u = self._l2normalize(K.dot(v, W))
+        return u, v
+    
+    def build(self, input_shape):
+        if self.data_format == 'channels_first':
+            channel_axis = 1
+        else:
+            channel_axis = -1
+        if input_shape[channel_axis] is None:
+            raise ValueError('The channel dimension of the inputs '
+                             'should be defined. Found `None`.')
+        input_dim = input_shape[channel_axis]
+        kernel_shape = self.kernel_size + (input_dim, self.filters)
+
+        self.kernel = self.add_weight(shape=kernel_shape,
+                                      initializer=self.kernel_initializer,
+                                      name='kernel',
+                                      regularizer=self.kernel_regularizer,
+                                      constraint=self.kernel_constraint)
+        #Spectral Normalization
+        #Get kernel tensor shape
+        W_shape = self.kernel.shape.as_list()
+        #Flatten the Tensor
+        W_reshaped = K.reshape(self.kernel, [-1, W_shape[-1]])
+        #initialize u vector
+        u = K.random_normal_variable([1, W_shape[-1]], 0 ,1)
+
+        u,v = self.power_iteration(u, W_reshaped)
+        #Calculate Sigma
+        sigma=K.dot(v, W_reshaped)
+        sigma=K.dot(sigma, K.transpose(u))
+        #normalize it
+        W_bar = W_reshaped / sigma
+        #reshape weight tensor
+        W_bar = K.reshape(W_bar, W_shape)
+        #update weitht
+        self.kernel = W_bar
+        if self.use_bias:
+            self.bias = self.add_weight(shape=(self.filters,),
+                                        initializer=self.bias_initializer,
+                                        name='bias',
+                                        regularizer=self.bias_regularizer,
+                                        constraint=self.bias_constraint)
+        else:
+            self.bias = None
+        # Set input spec.
+        self.input_spec = InputSpec(ndim=self.rank + 2,
+                                    axes={channel_axis: input_dim})
+        self.built = True
